f59666eb221b803e552013ba3d31974c9ce6c672
Oliver Källerfors <idetorka@hotmail.com>
2022-08-18 20:04:07 +0200

    Added Ritual, Penance, Polo, atEveryone and HelloBot Commands

--- ProjectBot/Modules/Commands.cs
+++ ProjectBot/Modules/Commands.cs
@@ -1,5 +1,8 @@
-﻿using Discord;
+﻿using System.Diagnostics;
+using Discord;
 using Discord.Commands;
+
+
 
 namespace ProjectBot.Modules;
 
@@ -7,6 +10,10 @@ public class Commands : ModuleBase<SocketCommandContext>{
     [Command("Marco")]
     public async Task Ping(){
         await ReplyAsync("Polo");
+    }
+    [Command("Polo")]
+    public async Task Polo(){
+        await ReplyAsync("Breh... No");
     }
     [Command("Sacrifice")]
     public async Task Sacrifice(IGuildUser user = null){
@@ -24,11 +31,60 @@ public class Commands : ModuleBase<SocketCommandContext>{
     [Command("ChangeBotNickname")]
     public async Task ChangeBotNickname(string nickname){
         await Context.Guild.CurrentUser.ModifyAsync(x => {
-            x.Nickname = nickname + "bot";
+            x.Nickname = nickname + " bot";
         });
         await ReplyAsync($"Changed my nickname to {nickname}");
     }
+    [Command("Ritual")]
+    public async Task Ritual(){
+       
+        await ReplyAsync(@"
+            Eko, eko, azarak. Eko, eko, zomelak.
+            Bagabi lacha bachabe, Lamac cahi achababe.
+            Karrellyos.
+            Lamac lamac bachalyas.
+            Cabahagy sabalyos. Baryolos.
+            Lagoz atha cabyolas. Smnahac atha famolas.
+            Hurrahya.");
+    }
     
+    [Command("Penance")]
+    public async Task Penance(){
+        var id=  Context.Message.Author.Id;
+        var userName = Context.Guild.GetUser(id).Nickname;
+        await (ReplyAsync($"{userName} lashes themself with a whip"));
+            
+    }
     
+    //Command that @everyone
+    [Command("atEveryone")]
+    public async Task Everyone(){
+        await ReplyAsync("@everyone\r\n"+
+                         "You Have Been Summoned For A Grand Ritual!\r\n\r\n"+
+                         "You Are To Perform The Following Rite:\r\n" +
+                         "   1. Join the lounge\r\n" +
+                         "   2. Write 666 Sacrifice @'Jesper\r\n" +
+                         "   3. Finish the project.\r\n");
+    }
 
+    [Command("HelloBot")]
+    public async Task HelloBot(){
+        var id=  Context.Message.Author.Id;
+        var userName = Context.Guild.GetUser(id).DisplayName;
+        Console.WriteLine(userName);
+        var helloUser =  $@"Console.WriteLine(""Hello {{{userName}}}"");";
+        await ReplyAsync($@"
+```cs
+public static class Program
+{{
+    public static void Main(string[] args)
+    {{
+         Console.WriteLine(""Hello {userName}!"");
+    }}
+}}
+```");
+    }
+    
+    
+    
 }
